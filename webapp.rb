0093569100d462f08295df8d204928e3895ca2e0
hiko1129 <wiz69gate4@hotmail.co.jp>
2016-06-28 16:57:18 +0900

    [update]

--- webapp.rb
+++ webapp.rb
@@ -1,10 +1,12 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 require_relative 'nega_posi_analysis'
 require_relative 'table_setter'
 
 
 get '/' do
+  @value = 'none'
   haml :index
 end
 
@@ -14,5 +16,6 @@ post '/' do
   p_o_s_table = set_table()
   analyzed_data = text_analyze(text)
   @value = calculate_value(p_o_s_table, analyzed_data)
+  @analyzed_data = analyzed_data.to_json
   haml :result
 end
